ade8e9c159ec1f6d2482a75964e7a4b3bc3d544b
Rob Purcell <borllecrup@me.com>
2012-11-25 22:40:57 +0000

    Add support for Scala compilation

---
+++ src/scala/com/robbyp/coffecake/Test.scala
@@ -0,0 +1,12 @@
+package com.robbyp.coffecake
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rob
+ * Date: 25/11/2012
+ * Time: 22:23
+ * To change this template use File | Settings | File Templates.
+ */
+case class Test(param1: Int, param2: Int) {
+
+}
